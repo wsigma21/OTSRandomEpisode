e0612c6d607d2924dca0194adccae15406db3526
wsigma21 <wallstlbl@gmail.com>
2024-11-01 23:14:14 +0900

    デプロイ用設定追加

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES
+        ? "randomEpisode"
+        : "./",
   plugins: [react()],
 })
