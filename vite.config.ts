4486a24ab1427bd26904fe1f85a80e7212f2da3e
wsigma21 <wallstlbl@gmail.com>
2024-11-01 23:20:14 +0900

    bite.configのリポジトリ名修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.GITHUB_PAGES
-        ? "randomEpisode"
+        ? "OVSRandomEpisode"
         : "./",
   plugins: [react()],
 })
