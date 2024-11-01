f6a801e328010a09fd78102467b8af68397e50ce
wsigma21 <wallstlbl@gmail.com>
2024-11-01 23:43:24 +0900

    GitHub Actions試行

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/OVSRandomEpisode/',
+  base: process.env.GITHUB_PAGES ? 'REPOSITORY_NAME' : './',
   plugins: [react()],
 })
